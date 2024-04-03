5957f1a0c3600265633f31e8a57a45c94180a8cc
Night <dranhata@gmail.com>
2024-04-03 22:46:12 +0700

    Add user change password API

--- backend/src/main/java/com/nctcompany/nct03/controller/UserController.java
+++ backend/src/main/java/com/nctcompany/nct03/controller/UserController.java
@@ -1,18 +1,21 @@
 package com.nctcompany.nct03.controller;
 
 import com.nctcompany.nct03.constant.SecurityConstants;
+import com.nctcompany.nct03.dto.user.ChangePasswordRequest;
 import com.nctcompany.nct03.dto.user.UserResponse;
+import com.nctcompany.nct03.model.User;
 import com.nctcompany.nct03.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequestMapping("/v1/users")
 @RestController
@@ -41,4 +44,24 @@ public class UserController {
         String token = jwt.substring(7);
         return ResponseEntity.ok(userService.getUserFromToken(token));
     }
+
+    @Operation(
+            summary = "Read User Profile"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "Http Status 204 No Content"
+    )
+    @SecurityRequirement(
+            name = "Bear Authentication"
+    )
+    @PatchMapping("/change-password")
+    public ResponseEntity<?> changePassword(
+            @Valid @RequestBody ChangePasswordRequest request,
+            Principal loginedUser
+            ){
+        userService.changePassword(request, loginedUser);
+        return ResponseEntity.noContent().build();
+    }
+
 }
